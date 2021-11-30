7e483da133ed59ed52745bd336c733ff13ff4a6d
Edwar Malpica <edwar.malpica@uptc.edu.co>
2021-11-30 17:20:04 -0500

    Implementacion de desaparecer objetos en javascript

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 console.log("script loader")
 var form1 = document.getElementById("form-1");
-var content = document.getElementById("resultForm");
+
+var sectionOne = document.getElementById("Contenedor1");
+var sectionTwo = document.getElementById("DeFi");
+var sectionTree = document.getElementById("AplicacionesDeFi");
+var sectionTFour = document.getElementById("RedesDePago");
+var sectionFive = document.getElementById("form-1");
+var sectionSix = document.getElementById("footer1");
 form1.addEventListener("submit" , function(event){
-	var name = form1.elements["name"].value;
+	
 	event.preventDefault();
 	const data = Object.fromEntries(new FormData(event.target).entries());
-	console.log(event);
-	console.log("submit")
+	sectionOne.style.visibility = "hidden";
+	sectionTwo.style.visibility = "hidden";
+	sectionTree.style.visibility = "hidden";
+	sectionTFour.style.visibility = "hidden";
+	sectionFive.style.visibility = "hidden";
+	sectionSix.style.visibility = "hidden";
+	
+	console.log("submit");
 });
